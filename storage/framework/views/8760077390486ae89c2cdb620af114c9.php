b76a31a423c98d959407cca6f25b25c68090eb35
Andre-Andrade <andrejardim7@gmail.com>
2023-06-14 13:13:56 -0300

    subindo arquivos

---
+++ storage/framework/views/8760077390486ae89c2cdb620af114c9.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel 9 Vite with Install Vue 3 - Techsolutionstuff</title>
+ <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" value="<?php echo e(csrf_token()); ?>"/>
+        <title>How To Create Laravel 9 With Vue JS CRUD Operation - Techsolutionstuff</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+	<?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+</head>
+	<div id="app"></div>
+	<?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+
+
+
+</body>
+</html>
+<?php /**PATH C:\xampp81\htdocs\testeoo\laravel-9-vite-vue3-example - backend\resources\views/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e913492904542aaaddbe2e342f3729d3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel 9 Vite with Install Vue 3 - Techsolutionstuff</title>
+
+	<?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+</head>
+<body>dsds
+	<div id="app"></div>
+
+	<?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\testeoo\laravel-9-vite-vue3-example - Copia\resources\views/welcome.blade.php ENDPATH**/ ?>
